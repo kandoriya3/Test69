f9f73c1238e769255dcf0b06f4084e187b6c9eaa
kandoriya3 <86428593+kandoriya3@users.noreply.github.com>
2022-12-21 00:12:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const removeBg = require('remove.bg');
 
 removeBg.apiKey = 'RT1QdU9hA1vytrc22JL5GBrk';
 
-removeBg.removeBackgroundFromImage('image.jpg', 'output.png', (error, result) => {
+removeBg.removeBackgroundFromImage('input.jpg', 'output.png', (error, result) => {
   if (error) {
     console.log(error);
   } else {
